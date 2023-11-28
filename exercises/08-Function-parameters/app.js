5eca0b7d88bf26e8b0f26a63359d03c8c3a32e77
Federico Resano <123256603+fede1525@users.noreply.github.com>
2023-11-28 10:24:35 +0000

    commit

--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,5 +1,8 @@
 // Your code goes here:
-
+function renderPerson(name, date, color, age, gender){
+    let fullString = name + " is a " + age + " years old " + gender + " born in " + date + " with " + color + " eyes";
+    return fullString;
+}
 
 
 ///*** Do not edit below this line ***///
--- exercises/10-Remove-vowels/app.js
+++ exercises/10-Remove-vowels/app.js
@@ -1,5 +1,10 @@
 // Your code goes here
-
+const rapid = (str) => {
+    const regex = /[aeiou]/gi;
+    str.replace(regex, "");
+    str = str.toUpperCase();
+    return str;
+};
 
 // Work above this line; do not change code below
 let str = "John";
